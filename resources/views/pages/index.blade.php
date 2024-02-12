de3b11fd236551efae717bdaf33f98059e7bf086
CosimoA <cosimo.archip@gmail.com>
2024-02-12 17:33:35 +0100

    FEAT: create & style comics section with forich cicle.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,5 +4,31 @@
 @endsection
 @section('content')
 
-    {{-- <h1>Hello, World!</h1> --}}
+    <section id="comics">
+        <div class="container">
+            <div class="title-section">
+                <h2 class="uppercase"> current series </h2>
+            </div>
+            <div class="box-comics">
+                @foreach ($comics as $comic)
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                        </div>
+                        <div class="card-title">
+                            <p class="uppercase">
+                                {{ $comic['title'] }}
+                            </p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="button">
+                <div class="btn-load">
+                    <a class="uppercase" href="#"> load more</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
